fa6635e46743102d5726b4488e4aead04df357de
hirayclay <2425875132@qq.com>
2018-07-26 11:18:34 +0800

    修复切面bug（空格导致的bug+_+）

--- app/src/main/java/com/hiray/MainActivity.kt
+++ app/src/main/java/com/hiray/MainActivity.kt
@@ -10,6 +10,7 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import com.hiray.aop.DebugLog
 import com.hiray.databinding.ActivityMainBinding
 import com.hiray.databinding.NavHeaderMainBinding
 import com.hiray.di.component.DaggerMainComponent
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var userviewModel: UserViewModel
 
+    @DebugLog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DaggerMainComponent.builder().appComponent((application as App).appComponent)
